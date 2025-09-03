df0c1a8507cccc08a11799e08e8b35c058b2a56f
Toko122 <toko.migine@gmail.com>
2025-09-03 16:58:24 +0400

    m

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,6 +5,7 @@ import axios from '../app/axios'
 import React, { useEffect, useState } from 'react'
 import { FaRegUserCircle } from 'react-icons/fa'
 import { FiArrowRight } from "react-icons/fi";
+import { ButtonLoading } from './ui/Loading';
 
 
 const Home = () => {
@@ -13,6 +14,8 @@ const Home = () => {
     const [visibleCount, setVisibleCount] = useState(5);
 
     const [loggedUser, setLoggedUser] = useState(null)
+
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         setLoggedUser(localStorage.getItem('token'))
@@ -23,12 +26,15 @@ const Home = () => {
 
     useEffect(() => {
         const fetchUser = async () => {
+            setLoading(true)
             try {
 
                 const res = await axios.get(`/users/allUsers`)
                 setUsers(res.data)
             } catch (err) {
                 console.log(err);
+            } finally {
+                setLoading(false)
             }
         }
         fetchUser()
@@ -38,29 +44,37 @@ const Home = () => {
     return (
         <div className='w-full min-h-screen pb-12 flex justify-center pt-28 bg-[#F4F2EE] md:px-8 px-4'>
             <div className='flex flex-col gap-4 w-full items-center'>
+
                 {
-                    visibleUsers.map((user) => (
-                        <Link href={loggedUser ? `/user/${user._id}` : '/auth/login'} className='flex justify-between items-center cursor-pointer hover:bg-[#e7e7e7] transition duration-200 bg-white py-3 px-4 rounded-lg w-full max-w-3xl pr-6'>
-                            <div className='flex gap-4 items-center'>
-                                {
-                                    user.imageUrl ? (
-                                        <img src={user.imageUrl} className='w-[60px] h-[60px] rounded-full object-cover' />
-                                    ) : (
-                                        <FaRegUserCircle className='w-[60px] h-[60px] rounded-full ' />
-                                    )
-                                }
+                    loading ? (
+                        <ButtonLoading />
+                       ) :
+                        (
+                            
+                                visibleUsers.map((user) => (
+                                    <Link href={loggedUser ? `/user/${user._id}` : '/auth/login'} className='flex justify-between items-center cursor-pointer hover:bg-[#e7e7e7] transition duration-200 bg-white py-3 px-4 rounded-lg w-full max-w-3xl pr-6'>
+                                        <div className='flex gap-4 items-center'>
+                                            {
+                                                user.imageUrl ? (
+                                                    <img src={user.imageUrl} className='w-[60px] h-[60px] rounded-full object-cover' />
+                                                ) : (
+                                                    <FaRegUserCircle className='w-[60px] h-[60px] rounded-full ' />
+                                                )
+                                            }
 
-                                <div className='flex flex-col gap-2'>
-                                    <h1 className='font-semibold text-[18px]'>{user.username}</h1>
-                                    <p className='text-black'>{user.job}</p>
-                                </div>
+                                            <div className='flex flex-col gap-2'>
+                                                <h1 className='font-semibold text-[18px]'>{user.username}</h1>
+                                                <p className='text-black'>{user.job}</p>
+                                            </div>
 
-                            </div>
-                            <div className=''>
-                                <FiArrowRight className='text-2xl' />
-                            </div>
-                        </Link>
-                    ))
+                                        </div>
+                                        <div className=''>
+                                            <FiArrowRight className='text-2xl' />
+                                        </div>
+                                    </Link>
+                                ))
+                            
+                        )
                 }
 
                 {
